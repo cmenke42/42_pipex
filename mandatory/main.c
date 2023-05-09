3d67906b0ace0a2a3d9aca4ce6b79c6bdaf91980
cmenke <cmenke@student.42wolfsburg.de>
2023-05-09 21:37:27 +0000

    added quote check and block_count for splitting cmd line

--- mandatory/main.c
+++ mandatory/main.c
@@ -3,10 +3,66 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cmenke <cmenke@student.42.fr>              +#+  +:+       +#+        */
+/*   By: user <user@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/09 20:08:33 by cmenke            #+#    #+#             */
-/*   Updated: 2023/05/09 20:08:34 by cmenke           ###   ########.fr       */
+/*   Updated: 2023/05/09 21:35:27 by user             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "pipex.h"
+
+bool	ft_check_quote_amt(char *cmd_string)
+{
+	int	i;
+	int	single_quotes;
+
+	i = 0;
+	single_quotes = 0;
+	while (cmd_string[i])
+	{
+		if (cmd_string[i] == '\'')
+			single_quotes++;
+		i++;
+	}
+	if (single_quotes % 2 == 0)
+		return (true);
+	return (false);
+}
+
+int	ft_count_cmd_blocks(char *cmd_string, char c)
+{
+	int	block_count;
+	int	i;
+
+	block_count = 0;
+	i = -1;
+	while (cmd_string[++i])
+	{
+		if (cmd_string[i] != c && cmd_string[i] != '\'')
+		{
+			block_count++;
+			while (cmd_string[i] && cmd_string[i] != c && cmd_string[i] != '\'')
+				i++;
+		}
+		if (cmd_string[i] == '\'')
+		{
+			while (cmd_string[++i] && cmd_string[i] != '\'')
+				;
+			if (cmd_string[i])
+				i++;
+		}
+		if (!cmd_string[i])
+			break ;
+	}
+	return (block_count);
+}
+
+
+int	main(int argc, char **argv, char **envp)
+{
+	if(ft_check_quote_amt(argv[1]) == false)
+		return (1);
+	ft_printf("block count:%d\n", ft_count_cmd_blocks(argv[1], ' '));
+	ft_printf("still alive\n");
+}
