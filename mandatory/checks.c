ff58cebace74a014b46a2f33f4809f900236e776
cmenke <cmenke@student.42wolfsburg.de>
2023-05-10 12:00:16 +0000

    added double quotes to check quotes function

--- mandatory/checks.c
+++ mandatory/checks.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   checks.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cmenke <cmenke@student.42.fr>              +#+  +:+       +#+        */
+/*   By: user <user@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/10 18:08:06 by cmenke            #+#    #+#             */
-/*   Updated: 2023/05/10 22:38:08 by cmenke           ###   ########.fr       */
+/*   Updated: 2023/05/10 11:59:50 by user             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,20 @@ bool	ft_check_quote_amt(char *cmd_string)
 {
 	int	i;
 	int	single_quotes;
+	int	double_quotes;
 
 	i = 0;
 	single_quotes = 0;
+	double_quotes = 0;
 	while (cmd_string[i])
 	{
 		if (cmd_string[i] == '\'')
 			single_quotes++;
+		else if (cmd_string[i] == '"')
+			double_quotes++;
 		i++;
 	}
-	if (single_quotes % 2 == 0)
+	if (single_quotes % 2 == 0 && double_quotes % 2 == 0)
 		return (true);
 	return (false);
 }
