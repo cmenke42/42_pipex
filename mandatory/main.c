dbeca650a455b93c768ef74a144b720153318a5e
Carlos Menke <cmenke@c2s18d116.42wolfsburg.de>
2023-05-10 16:59:58 +0200

    block couting seems to work

--- mandatory/main.c
+++ mandatory/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: user <user@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: cmenke <cmenke@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/09 20:08:33 by cmenke            #+#    #+#             */
-/*   Updated: 2023/05/10 11:48:07 by user             ###   ########.fr       */
+/*   Updated: 2023/05/10 16:59:30 by cmenke           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,54 +30,22 @@ bool	ft_check_quote_amt(char *cmd_string)
 	return (false);
 }
 
-// // start with i = -1
-// int	ft_count_cmd_blocks(char *cmd_string, char c)
-// {
-// 	int	block_count;
-// 	int	i;
-	
-// 	block_count = 0;
-// 	i = -1;
-// 	while (cmd_string[++i])
-// 	{
-// 		if (cmd_string[i] != c)
-// 		{
-// 			if (cmd_string[i] == c || block_count == 0)
-// 				block_count++;
-// 			if (cmd_string[i] == '\'')
-// 				while (cmd_string[++i] && cmd_string[i] != '\'')
-// 					;
-// 			else
-// 				while (cmd_string[i] && cmd_string[i] != c && cmd_string[i] != '\'')
-// 					i++;
-// 		}
-// 		if (!cmd_string[i])
-// 			break ;
-// 	}
-// 	return (block_count);
-// }
-
-int	ft_skip_quote_block(char*s, int i)
+void	ft_skip_quote_block(char*s, int *i)
 {
 	//skip first '
-	i++;
-	while (s[i] && s[i] != '\'')
-		i++;
-	return (i);
+	*i += 1;
+	while (s[*i] && s[*i] != '\'')
+		*i += 1;
 }
 
-bool	ft_search_for_valid_char(char*s, char c)
+void ft_skip_to_next_delim(char *s, char c, int *i)
 {
-	int i;
-
-	i = 0;
-	while (s[i])
+	while(s[*i] && s[*i] != c)
 	{
-		if(s[i] != c && s[i] != '\'')
-			return (true);
-		i++;
+		if (s[*i] == '\'')
+			ft_skip_quote_block(s, i);
+		*i += 1;
 	}
-	return (false);
 }
 
 //start with i = -1
@@ -90,76 +58,67 @@ int	ft_count_cmd_blocks(char *s, char c)
 	i = -1;
 	while (s[++i])
 	{
-		ft_printf("char:%c\n", s[i]);
-		if (s[i] != c)
+		if(s[i] != c)
 		{
-			if (s[i] != '\'' || (i > 0 && 
-				(s[i - 1] == ' ' && s[i] == '\'' && s[i + 1] != '\''))
-				|| i == 0 && (s[i] == '\'' && s[i + 1] != '\''))
-			{
-				block_count++;
-				if (s[i] == '\'')
-					i = ft_skip_quote_block(s, i);
-				else
-				{
-					while (s[i] != c && s[i])
-						i++;
-				}
-			}
-			else
-				i = ft_skip_quote_block(s, i);
-			if (s[i] == '\0')
-				break ;
+			block_count++;
+			ft_skip_to_next_delim(s, c, &i);
 		}
+		if(!s[i])
+			break ;
 	}
-	if (block_count == 0 && ft_search_for_valid_char(s, c) == true)
-		block_count++;
 	return (block_count);
 }
-// //start with i = -1
-// int	ft_count_cmd_blocks(char *s, char c)
-// {
-// 	int	block_count;
-// 	int	i;
+
+char	**ft_get_cmd_blocks(char *s, char c, char **result)
+{
+	int		i;
+	int		j;
+	int		start;
 	
-// 	block_count = 0;
-// 	i = 0;
-// 	while(s[i])
-// 	{
-// 		if (s[i] != c)
-// 		{
-// 			if ((s[i] != c && s[i] != '\'')
-// 				|| (s[i] && s[i] == '\''
-// 				&& s[i + 1] != '\''))
-// 					block_count++;
-// 			while (s[i] && s[i] != c && s[i] != '\'')
-// 				i++;
-// 		}
-// 		else if (s[i] == '\'')
-// 		{
-		
-// 		}
-// 		if (!s[i])
-// 			break ;
-// 		// if (s[i] == c)
-// 		// {
-// 		// 	while (s[i] && s[i] == c && s[i] != '\'')
-// 		// 		i++;
-// 		// 	if ((s[i] != c && s[i] != '\'')
-// 		// 		|| (s[i] && s[i] == '\''
-// 		// 		&& s[i + 1] != '\''))
-// 		// 			block_count++;
-// 		// }
-// 	}
+	i = -1;
+	j = -1;
+	while (s[++i])
+	{
+		if(s[i] != c)
+		{
+			start = i;
+			ft_skip_to_next_delim(s, c, &i);
+			result[++j] = ft_substr(s, start, i - start);
+		}
+		if(!result[j])
+		{
+			perror("get cmd substrings - malloc");
+			while (j >= 0)
+				free(result[j--]);
+			free(result);
+			return (NULL);
+		}
+		if(!s[i])
+			break ;
+	}
+	return (result);
+}
 
-// 	return (block_count);
-// }
-
+char	**ft_get_cmd_and_cmd_args(char *s)
+{
+	char	**result;
+	result = (char **)malloc((ft_count_cmd_blocks(s, ' ') + 1) * sizeof(char *));
+	if (!result)
+		perror("get cmd args - malloc");
+	result = ft_get_cmd_blocks(s, ' ', result);
+	return (result);
+}
 
 int	main(int argc, char **argv, char **envp)
 {
 	if(ft_check_quote_amt(argv[1]) == false)
 		return (1);
-	ft_printf("block count:%d\n", ft_count_cmd_blocks(argv[1], ' '));
+	char **result;
+
+	result = ft_get_cmd_and_cmd_args(argv[1]);
+	int i = 0;
+	while (result && result[i])
+		ft_printf("args:%s\n", result[i++]);
+	ft_printf("\nblock count:%d\n", ft_count_cmd_blocks(argv[1], ' '));
 	ft_printf("still alive\n");
 }
