9bc5e9fc2c04fe7f0f4ccdf8575d31fad60a39c4
Carlos Menke <cmenke@c2s17d105.42wolfsburg.de>
2023-05-11 15:32:36 +0200

    added  double quote handling - and ignoring the other type inside

--- mandatory/remove_quotes.c
+++ mandatory/remove_quotes.c
@@ -3,35 +3,72 @@
 /*                                                        :::      ::::::::   */
 /*   remove_quotes.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: user <user@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: cmenke <cmenke@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/10 18:27:40 by cmenke            #+#    #+#             */
-/*   Updated: 2023/05/11 02:04:42 by user             ###   ########.fr       */
+/*   Updated: 2023/05/11 15:31:53 by cmenke           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-static char *ft_copy_without_quotes(char *s)
+static void	ft_skip_quote_block(char*s, int *i, char quote, int *cnt)
 {
-	int	i;
-	int	j;
-	int	counter;
-	char *result;
+	//skip first '
+	*i += 1;
+	while (s[*i] && s[*i] != quote)
+	{	*cnt += 1;
+		*i += 1;
+	}
+	if(s[*i] == quote)
+		*i += 1;
+}
+
+static int ft_count_chars(char *s)
+{
+	int i;
+	int counter;
 
 	counter = 0;
-	i = -1;	
-	while (s[++i])
-		if(s[i] != '\'' && s[i] != '"')
-			counter++;
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == '\'')
+			ft_skip_quote_block(s, &i, '\'', &counter);
+		else if (s[i] == '"')
+			ft_skip_quote_block(s, &i, '"', &counter);
+		if (!s[i])
+			break ;
+		counter++;
+		i++;
+	}
+	return (counter);
+}
+
+static char *ft_copy_without_quotes(char *s, int i, int j, int counter)
+{
+	char	*result;
+	char	quote;
+
+	counter = ft_count_chars(s);
 	result = malloc((counter + 1) * sizeof(char));
 	if(!result)
 		return (NULL);
-	i = -1;
-	j = 0;
-	while (s[++i])
-		if(s[i] != '\'' && s[i] != '"')
-			result[j++] = s[i];
+	while (s[i])
+	{
+		if(s[i] == '\'' || s[i] == '"')
+		{
+			if (s[i] == '\'')
+				quote = s[i++];
+			else if (s[i] == '"')
+				quote = s[i++];
+			while (s[i] && s[i] != quote)
+				result[j++] = s[i++];
+			if (!s[i++])
+				break ;
+		}
+		result[j++] = s[i++];
+	}
 	result[j] = '\0';
 	return (result);
 }
@@ -44,7 +81,7 @@ char **ft_remove_quotes(char **cmd_args)
 	i = 0;
 	while (cmd_args[i])
 	{
-		temp = ft_copy_without_quotes(cmd_args[i]);
+		temp = ft_copy_without_quotes(cmd_args[i], 0, 0, 0);
 		if(!temp)
 		{
 			perror("remove quotes - malloc");
@@ -55,6 +92,5 @@ char **ft_remove_quotes(char **cmd_args)
 		cmd_args[i] = temp;
 		i++;
 	}
-	ft_printf("string:%s\n", cmd_args[i]);
 	return (cmd_args);
 }
